489a2db8df49ea07622601a752f2aea5c73b5a79
Andrey Picado Arias <andreypcd@gmail.com>
2020-09-28 13:51:05 -0600

    Java Maven de las clases

---
+++ alien1/src/main/java/pack/poo1/MainView.java
@@ -0,0 +1,21 @@
+package pack.poo1;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route
+@PWA(name = "AlienJet", shortName = "AlienJet")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+}
---
+++ alien1/src/main/java/pack/poo1/Principal.java
@@ -0,0 +1,35 @@
+package pack.poo1;//Paquete por defecto
+//Importación de librerías
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    //Lista de productos
+    private static ArrayList<Producto> productos = new ArrayList<Producto>();
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> listaPrecios;
+        List<String> listaProductos;
+        /*
+         * opcion solo puede ser tres valores: -JETS -COMPLEMENTOS -MUNDOS
+         */
+        String opcion = "MUNDOS";
+        Categoria categoria = new Categoria();
+        categoria.leerCategoria(opcion);
+        listaPrecios = categoria.obtenerPrecios();
+        listaProductos = categoria.obtenerProductos();
+        System.out.println("***********\n" + "Lista de precios para " + opcion + " :\n" + listaPrecios + "\n*************");
+        System.out.println("Lista de productos:\n" + listaProductos);
+        for (int i = 0; i < listaPrecios.size(); i = i + 1) {
+            Producto producto = new Producto();//Nuevas instancias de productos
+            producto.crearProducto(listaPrecios.get(i), listaProductos.get(i), opcion);// int pprecio, String pnombre,String ptipo
+            productos.add(producto);
+        }
+        verProductos();
+    }
+    private static void verProductos() {
+        for(int i = 0; i < productos.size();i = i + 1){
+            productos.get(i).devolverAttr();
+        }
+    }
+}
